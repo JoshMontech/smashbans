c5e5ca3270e811f12f73c5b9c4dd623c85d14be1
Joshua Montgomery <JoshMontgomery@utexas.edu>
2019-07-05 11:12:38 -0500

    removed error

--- pages/index.js
+++ pages/index.js
@@ -17,15 +17,15 @@ const Index = props => (
   </Layout>
 );
 
-Index.getInitialProps = async function() {
-  // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  // const data = await res.json();
+// Index.getInitialProps = async function() {
+//   // const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+//   // const data = await res.json();
 
-  // console.log(`Show data fetched. Count: ${data.length}`);
+//   // console.log(`Show data fetched. Count: ${data.length}`);
 
-  // return {
-  //   shows: data.map(entry => entry.show)
-  // };
-};
+//   // return {
+//   //   shows: data.map(entry => entry.show)
+//   // };
+// };
 
 export default Index;
